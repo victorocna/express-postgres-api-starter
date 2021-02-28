01c71f787c5cd5f614d71e626d6030927c3417c2
victorocna <victorocnarescu@gmail.com>
2021-02-28 16:42:35 +0200

    remove temp cookies

--- controllers/identity/profile.js
+++ controllers/identity/profile.js
@@ -15,8 +15,5 @@ module.exports = async (req, res) => {
     throw error(404, 'Profile not found');
   }
 
-  // set a temporary test cookie
-  res.cookie('current_timestamp', +Date.now());
-
   return res.status(200).json(profile);
 };
