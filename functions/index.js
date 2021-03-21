bc477665c8984c39e2cbc08f63e8d5639875755a
victorocna <victorocnarescu@gmail.com>
2021-03-21 14:11:28 +0200

    update router

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
+const coffee = require('./coffee');
 const error = require('./error');
 const randomHash = require('./random-hash');
 const removeRefreshTokenCookie = require('./remove-refresk-token-cookie');
 
 module.exports = {
+  coffee,
   error,
   randomHash,
   removeRefreshTokenCookie,
