53eea261ab985e6c7a36ddca24b94823bda5872c
victorocna <victorocnarescu@gmail.com>
2021-03-22 11:17:36 +0200

    update seeds

--- db/functions/get-identity-by-email.js
+++ db/functions/get-identity-by-email.js
@@ -1,8 +1,8 @@
 const { knex } = require('..');
 
-const getIdentityByName = async (name) => {
+const getIdentityByEmail = async (name) => {
   const identity = await knex('identities').select('id').where('name', name).first();
   return identity.id;
 };
 
-module.exports = getIdentityByName;
+module.exports = getIdentityByEmail;
--- db/functions/index.js
+++ db/functions/index.js
@@ -1,5 +1,5 @@
-const getIdentityByName = require('./get-identity-by-name');
+const getIdentityByEmail = require('./get-identity-by-email');
 
 module.exports = {
-  getIdentityByName,
+  getIdentityByEmail,
 };
