47872eddc07a9a47a204842a2b154dc67efa091a
victorocna <victorocnarescu@gmail.com>
2021-03-29 11:57:12 +0300

    update paginate function

--- functions/paginate.js
+++ functions/paginate.js
@@ -1,22 +1,16 @@
 const Knex = require('knex');
 
 module.exports.attachPaginate = function attachPaginate() {
-  function paginate(query) {
-    let { per_page: perPage = 30, page: currentPage = 1 } = query;
-
+  function paginate({ per_page: perPage = 30, page: currentPage = 1 }) {
     if (isNaN(perPage)) {
       throw new Error('Paginate error: perPage must be a number.');
     }
-
     if (isNaN(currentPage)) {
       throw new Error('Paginate error: currentPage must be a number.');
     }
-
     if (currentPage < 1) {
       currentPage = 1;
     }
-
-    const offset = (currentPage - 1) * perPage;
 
     const countQuery = new this.constructor(this.client)
       .count('* as total')
@@ -25,6 +19,7 @@ module.exports.attachPaginate = function attachPaginate() {
       .debug(this._debug);
 
     // This will paginate the data itself
+    const offset = (currentPage - 1) * perPage;
     this.offset(offset).limit(perPage);
 
     return this.client.transaction(async (trx) => {
