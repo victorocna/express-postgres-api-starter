af04b4253d7a2957509e9e503d313071843bb28a
victorocna <victorocnarescu@gmail.com>
2022-03-14 18:08:09 +0200

    update eslint rules

--- db/seeds/001_identities.js
+++ db/seeds/001_identities.js
@@ -1,14 +1,13 @@
+/* eslint-disable no-console */
 const identities = require('../resources/identities');
 
 exports.seed = async (knex) => {
   try {
-    // eslint-disable-next-line
     console.log('Planting seeds for identities');
 
     const seeds = await identities();
     await knex('identities').insert(seeds);
 
-    // eslint-disable-next-line
     console.log('✓');
   } catch (err) {
     console.warn('Error! Cannot insert identities');
--- examples/db/seeds/002_todos.js
+++ examples/db/seeds/002_todos.js
@@ -1,14 +1,13 @@
+/* eslint-disable no-console */
 const todos = require('../../../db/resources/todos');
 
 exports.seed = async (knex) => {
   try {
-    // eslint-disable-next-line
     console.log('Planting seeds for todos');
 
     const seeds = await todos();
     await knex('todos').insert(seeds);
 
-    // eslint-disable-next-line
     console.log('✓');
   } catch (err) {
     console.warn('Error! Cannot insert todos');
