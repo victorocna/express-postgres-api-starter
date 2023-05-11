66e8d12dc7e09e268ab052cea4288d193498e03a
victorocna <victorocnarescu@gmail.com>
2023-05-11 20:47:05 +0300

    add cookie domain

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const coffee = require('./coffee');
 const error = require('./error');
 const falsy = require('./falsy');
 const randomHash = require('./random-hash');
-const removeRefreshTokenCookie = require('./remove-refresk-token-cookie');
+const removeRefreshTokenCookie = require('./remove-refresh-token-cookie');
 const attachPaginate = require('./paginate');
 
 module.exports = {
